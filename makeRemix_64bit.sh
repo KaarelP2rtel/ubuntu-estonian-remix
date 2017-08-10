59e938e8fd4b4ef9e83408c5f33ca4ed6398304d
Edmund Laugasson <edmund.laugasson@gmail.com>
2017-08-10 23:12:02 +0300

    Update makeRemix_64bit.sh

--- makeRemix_64bit.sh
+++ makeRemix_64bit.sh
@@ -298,7 +298,7 @@ ENDSCRIPT
 cat > edit/tmp/libreoffice.sh << ENDSCRIPT
 
 # full system upgrade and newest libreoffice
-add-apt-repository -y ppa:libreoffice/ppa && apt update && apt full-upgrade -y && apt -y install libreoffice libreoffice-help-et libreoffice-l10n-et libreoffice-pdfimport libreoffice-nlpsolver libreoffice-ogltrans libreoffice-report-builder libreoffice-style-galaxy libreoffice-templates && apt -y remove libreoffice-style-tango && ldconfig && dpkg --configure -a && apt clean
+add-apt-repository -y ppa:libreoffice/ppa && apt update && apt full-upgrade -y && apt -y install libreoffice libreoffice-help-et libreoffice-l10n-et libreoffice-pdfimport libreoffice-nlpsolver libreoffice-ogltrans libreoffice-report-builder libreoffice-style-galaxy libreoffice-templates libreoffice-systray && apt -y remove libreoffice-style-tango libreoffice-style-breeze && ldconfig && dpkg --configure -a && apt clean
 ENDSCRIPT
 
 
