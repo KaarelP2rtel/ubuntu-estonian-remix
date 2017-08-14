88f274c450dbe84d2033bcca0a94904af6c0005b
Edmund Laugasson <edmund.laugasson@gmail.com>
2017-08-14 15:10:28 +0300

    Update makeRemix_64bit.sh

--- makeRemix_64bit.sh
+++ makeRemix_64bit.sh
@@ -105,9 +105,6 @@ export KIDS_PACKAGES="tuxpaint tuxpaint-config tuxpaint-plugins-default tuxtype 
 #name for Estonian Speller file in current directory
 export ESTONIAN_SPELLER="oofslinget-addon-estobuntu_4.1-0_all.deb"
 
-#name for the Estonian ID-card plugin for Caja file manager in MATE desktop environment
-export CAJA_ID_CARD_PLUGIN="caja-qdigidoc.py"
-
 #------
 #Check environment and make selections
 #------
@@ -351,6 +348,71 @@ apt -y autoremove --purge ${REMOVE_PACKAGES}
 
 ENDSCRIPT
 
+cat > edit/tmp/caja-qdigidoc.py <<EOF
+# Estonian ID-card plugin for Caja file manager in MATE desktop environment
+#
+# QDigiDoc Caja Extension
+#
+# Copyright (C) 2010  Erkko Kebbinau
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc, 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+import os
+import urllib
+import gettext
+import locale
+from gi.repository import Caja, GObject, Gio
+APP = 'nautilus-qdigidoc'
+class OpenDigidocExtension(GObject.GObject, Caja.MenuProvider):
+    def __init__(self):
+        pass
+    def _open_client(self, paths):
+        args = ""
+        for path in paths:
+            args += "\"%s\" " % path
+        cmd = ("qdigidocclient " + args + "&")
+        os.system(cmd)
+    def menu_activate_cb(self, menu, paths):
+        self._open_client(paths)
+    def valid_file(self, file):
+        return file.get_file_type() == Gio.FileType.REGULAR and file.get_uri_scheme() == 'file'
+    def get_file_items(self, window, files):
+        paths = []
+        for file in files:
+            if self.valid_file(file):
+                path = urllib.unquote(file.get_uri()[7:])
+                paths.append(path)
+        if len(paths) < 1:
+            return
+        locale.setlocale(locale.LC_ALL, '')
+        gettext.bindtextdomain(APP)
+        gettext.textdomain(APP)
+        _ = gettext.gettext
+        tooltip_message = gettext.ngettext('Sign selected file with Digidoc3 Client',
+                                           'Sign selected files with Digidoc3 Client',
+                                           len(paths))
+        item = Caja.MenuItem(
+            name="OpenDigidocExtension::DigidocSigner",
+            label=_('Sign digitally'),
+            tip=tooltip_message
+        )
+        item.set_property('icon', 'qdigidoc-client')
+        item.connect('activate', self.menu_activate_cb, paths)
+        return item,
+EOF
+
 cat > edit/tmp/libreoffice.sh << ENDSCRIPT
 
 # full system upgrade and newest libreoffice
@@ -395,7 +457,7 @@ apt -y install  ${KIDS_PACKAGES}
 if [ "$desktop_name" = "UNITY" ]; then
   echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections && apt install ubuntu-restricted-extras -y && apt clean
 elif [ "$desktop_name" = "MATE" ]; then
-  echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections && apt install ubuntu-restricted-extras -y && apt clean && mkdir -p /usr/share/caja-python/extensions/ && cp $CAJA_ID_CARD_PLUGIN /usr/share/caja-python/extensions/
+  echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections && apt install ubuntu-restricted-extras -y && apt clean && mkdir -p /usr/share/caja-python/extensions/ && cp edit/tmp/caja-qdigidoc.py /usr/share/caja-python/extensions/
 elif [ "$desktop_name" = "GNOME" ]; then
   echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections && apt install ubuntu-restricted-extras -y && apt clean && apt purge *lightdm* libreoffice-style-tango -y
 elif [ "$desktop_name" = "KDE" ]; then
